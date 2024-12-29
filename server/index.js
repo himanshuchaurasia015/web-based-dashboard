c97ac3b88f7dab6025d490578fba2703d15ddd97
Himanshu Chaurasia <himanshuchaurasia015@gmail.com>
2024-12-29 21:25:55 +0530

    completed

--- server/index.js
+++ server/index.js
@@ -12,10 +12,13 @@ const path = require("path");
 app.use("/uploads", express.static("uploads"));
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/team", teamRoute);
 app.use("/api/member", memberRoute);
 app.use("/api/organization", organizationRoute);
+
+app.get("/", (req, res) => {
+  return res.send("<h1>welcome</h1>");
+});
 
 app.listen(3245, () => {
   console.log("server is listening at", 3245);
